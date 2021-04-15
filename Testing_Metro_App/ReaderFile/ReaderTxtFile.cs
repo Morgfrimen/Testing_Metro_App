6b3fb6637c9eb2c33b0f4092aebb526d16e39c26
Александр Дрожкин <Santay2009@yandex.ru>
2021-04-15 20:45:17 +0300

    Add logical in readerTxtfile

--- Testing_Metro_App/ReaderFile/ReaderTxtFile.cs
+++ Testing_Metro_App/ReaderFile/ReaderTxtFile.cs
@@ -1,27 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
+
+using static Testing_Metro_App.Logger.Logger;
 
 namespace Testing_Metro_App.ReaderFile
 {
-	internal static class ReaderTxtFile
+    internal static class ReaderTxtFile
     {
-        internal static bool ReadFile(string path, out IList<(uint, uint)> result)
+        internal static async Task<IList<(uint, uint)>> ReadFile(string path)
         {
-            result = new List<(uint, uint)>();
+            IList<(uint, uint)> result = new List<(uint, uint)>();
+
             try
             {
-                
                 using (StreamReader streamReader = new(path))
                 {
-                    
+                    uint numberLine = default;
+
+                    while (true)
+                    {
+                        string line = await streamReader.ReadLineAsync();
+                        line = line.Trim(' ');
+
+                        if (line is null) break;
+
+                        string[] stringArray = line.Split(" ");
+                        numberLine++;
+
+                        if (!uint.TryParse(stringArray[0], out uint item1) 
+                            && !uint.TryParse(stringArray[1], out uint item2))
+                            throw new ArgumentException
+                            (
+                                $"В чтении строки произошла ошибка: невозможно преобразовать строку в целое число (номер строки {numberLine})"
+                            );
+                    }
                 }
 
-                return true;
+                return result;
             }
             catch (Exception e)
             {
-                return false;
+                Instance.OnPrintError(e);
+
+                return null;
             }
         }
     }
