54f82a7c7054487b228022047b29c907e6ecaf57
Александр Дрожкин <Santay2009@yandex.ru>
2021-04-15 20:08:19 +0300

    Add model node

---
+++ Testing_Metro_App/Model/INodeData.cs
@@ -0,0 +1,10 @@
+﻿namespace Testing_Metro_App.Model
+{
+    internal interface INodeData
+    {
+        internal uint Index { get; init; }
+        internal INodeData NodeDataLast { get; }
+        internal INodeData NodeDataNextLeft { get; }
+        internal INodeData NodeDataNextRight { get; }
+    }
+}
---
+++ Testing_Metro_App/Model/Node.cs
@@ -0,0 +1,46 @@
+﻿namespace Testing_Metro_App.Model
+{
+    internal sealed class Node : INodeData
+    {
+#region Fields
+
+        private readonly INodeData _nodeDataLast;
+        private readonly INodeData _nodeDataNextLeft;
+        private readonly INodeData _nodeDataNextRight;
+
+#endregion
+
+#region Constructors
+
+        internal Node(INodeData nodeDataLast) => _nodeDataLast = nodeDataLast;
+
+        internal Node(INodeData nodeDataLast, INodeData nodeDataNextLeft, INodeData nodeDataNextRight) : this(nodeDataLast)
+        {
+            _nodeDataNextLeft = nodeDataNextLeft;
+            _nodeDataNextRight = nodeDataNextRight;
+        }
+
+#endregion
+
+#region Properties
+
+        INodeData INodeData.NodeDataLast
+        {
+            get => _nodeDataLast;
+        }
+
+        INodeData INodeData.NodeDataNextLeft
+        {
+            get => _nodeDataNextLeft;
+        }
+
+        INodeData INodeData.NodeDataNextRight
+        {
+            get => _nodeDataNextRight;
+        }
+
+        uint INodeData.Index { get; init; }
+
+#endregion
+    }
+}
