03f887cc69fe3dc5a89ca0c5a734ba14b1b08bca
Александр Дрожкин <Santay2009@yandex.ru>
2021-04-15 20:20:22 +0300

    Add preview ReaderTxtFile

---
+++ Testing_Metro_App/ReaderFile/ReaderTxtFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Testing_Metro_App.ReaderFile
+{
+	internal static class ReaderTxtFile
+    {
+        internal static bool ReadFile(string path, out IList<(uint, uint)> result)
+        {
+            result = new List<(uint, uint)>();
+            try
+            {
+                
+                using (StreamReader streamReader = new(path))
+                {
+                    
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
