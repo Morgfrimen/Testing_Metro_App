574d07dae1af7b4bc2a24781e73117a7f5263876
Александр Дрожкин <Santay2009@yandex.ru>
2021-04-15 20:10:50 +0300

    Add Wood class

---
+++ Testing_Metro_App/Model/Wood.cs
@@ -0,0 +1,8 @@
+﻿namespace Testing_Metro_App.Model
+{
+    internal sealed class Wood
+    {
+        private readonly INodeData _firstNode;
+        internal Wood(INodeData firstNode) { _firstNode = firstNode; }
+    }
+}
