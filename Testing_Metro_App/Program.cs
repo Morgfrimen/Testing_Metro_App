731b50e6a39547355e414c508396283c8a14ba18
Александр Дрожкин <Santay2009@yandex.ru>
2021-04-15 21:19:17 +0300

    reader TXT to run

--- Testing_Metro_App/Program.cs
+++ Testing_Metro_App/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace Testing_Metro_App
 {
@@ -7,20 +10,22 @@ namespace Testing_Metro_App
         static Program()
         {
 			Logger.Logger.Instance.PrintErrorEvents += Instance_PrintErrorEvents;
+			Console.ResetColor();
         }
 
 		private static void Instance_PrintErrorEvents(Exception obj)
 		{
 			Console.Clear();
             Console.Beep();
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.BackgroundColor = ConsoleColor.White;
 			Console.WriteLine(obj.Message);
         }
 
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
-		}
+            IList<(uint, uint)> readerList = await ReaderFile.ReaderTxtFile.ReadFile(Path.Combine("InputFile","InputDefault.txt"));
+        }
 	}
 }
