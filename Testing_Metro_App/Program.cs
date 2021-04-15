87bc4be4665028ce7862beee62768acd08669c23
Александр Дрожкин <Santay2009@yandex.ru>
2021-04-15 20:49:41 +0300

    Add static constructor

--- Testing_Metro_App/Program.cs
+++ Testing_Metro_App/Program.cs
@@ -4,6 +4,20 @@ namespace Testing_Metro_App
 {
 	class Program
 	{
+        static Program()
+        {
+			Logger.Logger.Instance.PrintErrorEvents += Instance_PrintErrorEvents;
+        }
+
+		private static void Instance_PrintErrorEvents(Exception obj)
+		{
+			Console.Clear();
+            Console.Beep();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.BackgroundColor = ConsoleColor.White;
+			Console.WriteLine(obj.Message);
+        }
+
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
