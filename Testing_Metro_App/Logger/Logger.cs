57109e5fc9ae4d986573e20c91d805b9c49500f6
Александр Дрожкин <Santay2009@yandex.ru>
2021-04-15 20:24:09 +0300

    Add Custom logger

---
+++ Testing_Metro_App/Logger/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Testing_Metro_App.Logger
+{
+    internal sealed class Logger
+    {
+        private Logger(){}
+
+        private static Logger _instance;
+
+        internal static Logger Instance
+        {
+            get => _instance ??= new Logger();
+        }
+
+
+        private event Action<Exception> PrintError;
+    }
+}
